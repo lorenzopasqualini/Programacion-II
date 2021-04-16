8e74bceb37ae217a208bb266fa7e0b6e971ccbb3
lorenzopasqualini <lpasqualini@udesa.edu.ar>
2021-04-16 11:20:17 -0300

    Formularios profile-edit y product-add

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const productController= require('../controllers/productController');
+const profileController= require('../controllers/profileController');
 
 router.get('/', productController.product);
 router.get('/add', productController.add);
+router.get('/profile', profileController.profile);
+router.get('/profile/edit', profileController.edit);
 
 module.exports = router;
