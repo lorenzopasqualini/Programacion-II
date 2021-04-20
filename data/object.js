733c34f2517000990867033e5570e8f1cb7b4680
Tobias Faur <tobiasfaur@MacBook-Air-de-Tobias.local>
2021-04-20 11:11:06 -0300

    carousel banner

---
+++ data/object.js
@@ -0,0 +1,32 @@
+/*INSERT INTO table_name
+VALUES (value1, value2, value3, ...);
+
+
+INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
+VALUES ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+*/
+
+/*Deberán crear una base de datos que con:
+Una tabla de usuarios donde cada equipo decidirá qué datos tendrá su usuario. Cómo mínimo debe contar con:
+Un campo de texto.
+Un campo de fecha.
+Un campo numérico (además del id de la clave principal).
+
+Una tabla de productos que contenga al menos :
+ID del usuario que lo cargó.
+URL de la imagen.
+Nombre del producto
+Fecha de creación.
+Las claves foráneas que correspondan.
+
+Una tabla de comentarios que tenga al menos: 
+ID del producto sobre el cual se está comentando.
+ID del usuario que creó el comentario.
+Texto del comentario.
+Fecha de creación.
+Las claves foráneas que correspondan
+
+Deberán insertar al menos 5 usuarios, 10 productos y 4 comentarios para cada producto.
+Los campos de las tablas deben coincidir con la información solicitada en cada formulario creado en el punto 1.7.
+Deberán entregar un archivo con las sentencias SQL para la creación de las tablas y la inserción de datos o podrán optar por entregar un archivo con el dump de la base de datos para que los profesores podamos importarla y evaluar.
+*/
