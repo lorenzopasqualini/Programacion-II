118ce4904319487a4b1a0d0535d34c58b031efe0
lorenzopasqualini <lpasqualini@udesa.edu.ar>
2021-05-13 22:50:13 -0300

    Pequeñas correciones a la página de products

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const indexController= require('../controllers/indexController');
 
 router.get('/', indexController.index);
 
-// Resultados buscador
+// Buscador
 router.get('/search-results', (req, res) => {
     let idSearch= req.params.id;
     return res.render('search-results');
