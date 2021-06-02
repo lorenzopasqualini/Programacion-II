cb44e9749aff3c865dfd217c8c69aa7b3b11af31
lorenzopasqualini <lpasqualini@udesa.edu.ar>
2021-06-02 16:02:58 -0300

    Ruta al perfil ahora se vincula con su respectivo id, uso del Locals

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,7 +1,20 @@
-const profileController={
-    profile: (req, res)=> res.render('profile'),
+const db= require('../database/models');
 
-    edit: (req, res)=> res.render('profile-edit'),
+const profileController={
+
+    profile: (req, res)=>{
+        let id= req.params.id;
+        db.User.findByPk(id)
+            .then(data=>{
+                return res.render('profile', {user: data})
+            })
+            .catch(error=>{
+                console.log(error);
+            })
+    },
+
+    edit: (req, res)=> res.render('profileEdit'),
+
 }
 
 module.exports= profileController;
